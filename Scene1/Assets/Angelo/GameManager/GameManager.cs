b1f939cbb2b7ef4b8532d08450061e3499d85b61
Xenith <a.guerrero@saedubai.com>
2017-02-22 12:51:02 +0400

    Audio
    Quick

---
+++ Scene1/Assets/Angelo/GameManager/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager
+{
+    private GameManager()
+    {
+
+    }
+
+    private static GameManager gameManagerInstance;
+
+    public static GameManager GameManagerInstance
+    {
+        get
+        {
+            if (gameManagerInstance == null)
+            {
+                gameManagerInstance = new GameManager();
+            }
+
+            return gameManagerInstance;
+        }
+    }
+
+
+}
