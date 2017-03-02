baf567184c1f2c090646a2d48ad0e503e93cacaf
Xenith <a.guerrero@saedubai.com>
2017-03-03 00:26:55 +0400

    stuff
    Signed-off-by: Xenith <a.guerrero@saedubai.com>

--- gpg210/Assets/Angelo/Behavior Tree/PatrolTest.cs
+++ gpg210/Assets/Angelo/Behavior Tree/PatrolTest.cs
@@ -4,17 +4,53 @@ using UnityEngine;
 
 public class PatrolTest : MonoBehaviour
 {
-    public Vector3[] vector3Array;
+    public Transform targetTransform;
+    public List<Transform> wayPoints = new List<Transform>();
+    public float distanceFromAgentToWaypoint;
+    public float rotationSpeed;
+    public Vector3 directionToWaypoint;
+    public int i = 0;
+
+    public Vector3 desiredVelocity;
+    public Vector3 steering;
+    public float maxVelocity = 5;
+    public Rigidbody rb;
 
     private void Awake()
     {
-        vector3Array = new Vector3[4];
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
+        rotationSpeed = 3f * Time.deltaTime;
 
+        targetTransform = wayPoints[i];
+
+        distanceFromAgentToWaypoint = Vector3.Distance(this.transform.position, targetTransform.position);
+
+        Debug.Log(directionToWaypoint);
+
+        directionToWaypoint = Vector3.Normalize(targetTransform.position - transform.position);
+
+        Quaternion lookAt = Quaternion.LookRotation(directionToWaypoint);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, lookAt, rotationSpeed);
+
+        desiredVelocity = Vector3.Normalize(targetTransform.position - transform.position) * maxVelocity;
+        steering = desiredVelocity - rb.velocity;
+        rb.AddForce(steering);
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity);
+
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            i++;
+        }
+
+        if(i == wayPoints.Count)
+        {
+            i = 0;
+        }
     }
-
-
 }
