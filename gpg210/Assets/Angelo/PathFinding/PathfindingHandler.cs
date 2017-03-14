40ca05ae847dc2449e610a3cb2866c9a72035637
Xenith <a.guerrero@saedubai.com>
2017-03-14 17:06:59 +0400

    a
    Signed-off-by: Xenith <a.guerrero@saedubai.com>

--- gpg210/Assets/Angelo/PathFinding/PathfindingHandler.cs
+++ gpg210/Assets/Angelo/PathFinding/PathfindingHandler.cs
@@ -4,5 +4,9 @@ using UnityEngine;
 
 public class PathfindingHandler : MonoBehaviour
 {
-
+    public static List<PathfindingNode> CreateAPath(List<PathfindingNode> gameMap, PathfindingNode start, PathfindingNode end)
+    {
+        List<PathfindingNode> openList = new List<PathfindingNode>();
+        List<PathfindingNode> closedList = new List<PathfindingNode>();
+    }
 }
---
+++ gpg210/Assets/Angelo/PathFinding/PathfindingTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathfindingTest : MonoBehaviour
+{
+    public PathfindingMap temp;
+    public PathfindingNode start;
+    public PathfindingNode end;
+    public List<PathfindingNode> path;
+
+    private void Awake()
+    {
+        path = PathfindingHandler.CreateAPath(temp.map, start, end);
+    }
+}
