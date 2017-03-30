4c0aa06ec35e9c9c3c01613531bb2914eef01ab2
Cameronmo99 <cameronmo99@gmail.com>
2017-03-30 20:12:30 +0400

    Splashscreen

---
+++ gpg210/Assets/Cameron/SplashScreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SplashScreen : MonoBehaviour {
+
+    public Image Logo;
+    public Image Background;
+    public float fadetimeLogo;
+    public float fadetimeBackground;
+
+    // Use this for initialization
+    void Start ()
+    {
+        Logo.GetComponent<CanvasRenderer>().SetAlpha(1.0f);
+        Background.GetComponent<CanvasRenderer>().SetAlpha(1.0f);
+
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        Logo.CrossFadeAlpha(0f, fadetimeLogo, false);
+        Background.CrossFadeAlpha(0f, fadetimeBackground, false);
+
+    }
+}
